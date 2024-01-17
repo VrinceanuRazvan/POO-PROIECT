14071d578f83d7fb9f02debae941cc9751f77e8e
Razvan <vrinceanurazvan2003@gmail.com>
2024-01-17 17:24:05 +0200

    Pentru bife

--- main.cpp
+++ main.cpp
@@ -15,31 +15,5 @@ int main() {
 
     game.run();
 
-    try {
-        throw CustomError("CustomError: A custom error occurred.");
-    } catch (const MyException &e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-    }
-
-    try {
-        throw AnotherError();
-    } catch (const MyException &e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-    }
-
-    try {
-        throw CriticalError();
-    } catch (const MyException &e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-    }
-
-    try {
-        throw FileError("example.txt");
-    } catch (const MyException &e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-    }
-
-    return 0;
-
     return 0;
 }
